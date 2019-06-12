819e1810270c8ad37b229a2dce8460512cd38f76
Kenji <51339721+KayckKenji@users.noreply.github.com>
2019-06-12 16:42:21 -0300

    funcoes em javascript

---
+++ 203-js/script.js
@@ -0,0 +1,93 @@
+
+/*let idade = 105;
+
+idade = idade + 1;
+console.log(idade)*/
+
+/*let nome = "Ju";
+alert('O nome do usuário é: ${nome} e o cargo dele é: ${cargo} e o signo é: ${signo}');
+
+// CONSTRUIR UMA ESCADA CONSOLE
+// O MATERIAL DA ESCADA #
+// QUANTOS DEGRAUS ELX QUER
+
+// idade = idade + 1
+// idade += 1
+
+// SEGUNDA ETAPA 
+// PERGUNTAR MATERIAL QUE ELX QUER PARA A ESCADA*/
+
+// let filmes = [
+//     {titulo: "Harry Potter", idade: 10},
+//     {titulo: "A Fantástoca Fábrica de Chocolates", idade: 4},
+//     {titulo: "Velozes e Furiosos 2", idade: 16}
+// ]
+
+// console.log(filmes[0].idade);
+
+
+
+// let idade = prompt("Qual sua idade?")
+
+// let filmes = [
+//     {titulo: "Harry Potter", idade: 10},
+//     {titulo: "A Viagem de Chihiro", idade: 4},
+//     {titulo: "Velozes e Furiosos 2", idade: 16}
+// ]
+
+// let suaIdade = prompt("Qual sua idade?");
+
+// for(let i in filmes){
+//     if(suaIdade>= filmes [i].idade){
+//         console.log(filmes [i].titulo);
+//     }
+// } 
+
+
+// FUNCOES
+
+// let valor1= prompt("Digite o primeiro valor");
+// let valor2= prompt("Digite o segundo valor");
+
+//     function somar(valor1, valor2){
+//         console.log(valor1 + valor2);
+//     }
+//     somar(10, 4);
+//     somar(34, 56);
+//     somar(60, 40);
+
+
+// function somar(n1, n2, n3, n4){
+//     let media = (n1+n2+n3+n4)/4;
+//     console.log(`A média do Kenji é: ${media}`);
+// }
+// somar (9, 8, 7, 10);
+
+
+// function subtrair (n1, n2){
+//     let subtrair = (n1, n2);
+//     console.log(`A subtração é: ${subtrair}`)
+// }
+
+
+// function calc(a, b){
+//     console.log(a, b);
+// }
+
+// calc(1,'/',2)
+
+
+// arrow function calculadora = (a, b) =>{
+//         console.log(a+b);
+// }
+
+
+// function
+// function calculadora (a, b)
+//         console.log(a+b);
+
+
+// let fazerConta = function(a,b){
+//     console.log(a+b);
+// }
+
